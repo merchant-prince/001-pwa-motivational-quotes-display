60abed763a13a219032b70f1b4bf290105d5a6bb
Harivansh <hello@harivan.sh>
2022-04-20 18:31:25 +0400

    fix cursor pointer

--- src/components/MotivationalQuote.vue
+++ src/components/MotivationalQuote.vue
@@ -6,7 +6,7 @@ const props = defineProps<{ quote: string }>();
 
 <template>
   <article
-    class="bg-white w-1/2 p-8 shadow-lg rounded-lg font-cursive text-3xl text-gray-800"
+    class="bg-white w-1/2 p-8 shadow-lg rounded-lg font-cursive text-3xl text-gray-800 cursor-pointer"
   >
     {{ props.quote }}
   </article>
